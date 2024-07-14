a3842f09a834a1b571ee38937e9578093bfc3c57
Derek Greer <greerde@gmail.com>
2024-07-14 10:47:56 -0400

    Initial Commit

---
+++ SMR-Sports/SMR_SportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMR_SportsApp.swift
+//  SMR-Sports
+//
+//  Created by Derek Greer on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SMR_SportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
